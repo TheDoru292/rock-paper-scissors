355db79552bcfe15166393ba8edaede17589b82a
Doru <thedoruv2@gmail.com>
2022-01-09 12:14:09 +0100

    Added UI

--- script.js
+++ script.js
@@ -1,64 +1,69 @@
 let playerSelection;
 let computerSelection;
+let playerScore = 0;
+let npcScore = 0;
+let message = "";
 
-function computerChoice() {
-    let number = Math.floor(Math.random() * 3) + 1;
-    switch(number) {
-        case 1:
-            computerSelection = "Rock";
-            break;
-        case 2:
-            computerSelection = "Paper";
-            break;
-        case 3:
-            computerSelection = "Scissors";
-            break;
-        default:
-            console.log("Something went wrong...");
-    }
+const score = document.querySelector("#score");
+score.textContent = `${playerScore} : ${npcScore}`;
+
+const result = document.querySelector("#resultText");
+
+function playerChoice(selection) {
+    return playerSelection = selection;
 }
 
-function playerChoice() {
-    let choice = prompt("Please choose between: Rock, Paper, Scissors.").toLowerCase();
-    if(choice === "rock") {
-        playerSelection = "Rock";
-
-    } else if (choice === "paper") {
-        playerSelection = "Paper";
-
-    } else if (choice === "scissors") {
-        playerSelection = "Scissors"
-
-    } else {
-        console.log("Please enter a real value!");
-        playerChoice();
-    }
+function computerChoice() {
+    let random = Math.floor(Math.random() * 3);
+    (random === 0) ? computerSelection = "rock"
+    : (random === 1) ? computerSelection = "paper"
+    : (random === 2) ? computerSelection = "scissors"
+    : message = "Something went wrong";
 }
 
 function playRound(playerSelection, computerSelection) {
-    if((playerSelection === "Rock") && (computerSelection === "Scissors")) {
-        return("You win! Rock beats scissors.");
-    } else if((playerSelection === "Scissors") && (computerSelection === "Paper")) {
-        return("You win! Scissors beats paper.");
-    } else if((playerSelection === "Paper") && (computerSelection === "Rock")) {
-        return("You win! Paper beats rock!");
-    } else if((computerSelection === "Scissors") && (playerSelection === "Paper")) {
-        return("You lost! Scissors beats paper!");
-    } else if((computerSelection === "Rock") && (playerSelection === "Scissors")) {
-        return("You lost! Rock beats scissors!");
-    } else if((computerSelection === "Paper") && (playerSelection === "Rock")) {
-        return("You lost! Paper beats rock!");
-    } else {
-        return("You tied! " + computerSelection + " ties with " + playerSelection);
+
+    if(playerSelection === computerSelection) {
+        return message = "It was a draw!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "scissors" && computerSelection === "paper" ||
+        playerSelection === "paper" && computerSelection === "rock") {
+        playerScore += 1;
+        return message = `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else {
+        npcScore +=1;
+        return message = `You lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
 
 function game() {
-    computerChoice();
-    playerChoice();
-    console.log(playRound(computerSelection, playerSelection));
+   if(npcScore === 5) {
+        result.textContent = "Game over! You lose.";
+   } else if(playerScore === 5) {
+       result.textContent = "Game over! You win!";
+   } else {
+       playRound(playerSelection, computerSelection);
+   }
 }
 
-for(i = 0; i < 5; i++) {
-    game();
-}
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        // console.log(e.target.className);
+        playerChoice(e.target.className);
+        computerChoice();
+        game();
+
+        score.textContent = `${playerScore} : ${npcScore}`;
+        
+        if(npcScore ===  5) {
+            result.textContent = "Game over! You lose.";
+        } else if (playerScore === 5) {
+            result.textContent = "Game over! You Win!";
+        } else {
+            result.textContent = message;
+        }
+    });
+});
