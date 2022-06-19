e075f2f89b6b70e30cfc7f76d242fcc56073093d
TheDoru <thedoruv2@gmail.com>
2022-06-19 15:37:09 +0200

    Finish project

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let npcScore = 0;
+
 function computerPlay() {
     let number = Math.floor(Math.random() * 3);
     let choice = determinePlay(number);
@@ -20,25 +23,56 @@ function determinePlay(number) {
     }
 }
 
-function playerChoice() {
-    let choice = prompt("What would you like to choose? Rock, paper or scissors").toLowerCase();
-    return choice;
+function playRound(e) {
+    if(npcScore === 5 || playerScore === 5) return;
+    let playerChoice = e.target.classList.value;
+    let npcChoice = computerPlay();
+    checkForWinner(npcChoice, playerChoice);
+    updateScore();
 }
 
-function playRound() {
-    let npcChoice = computerPlay();
-    let userChoice = playerChoice();
-    alert(checkForWinner(npcChoice, userChoice));
+function updateScore() {
+    const player = document.querySelector("span.player")
+    const npc = document.querySelector("span.npc");
+
+    player.textContent = `${playerScore}`;
+    npc.textContent = `${npcScore}`
+}
+
+function createParagraph(text, logs) {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = text;
+    logs.append(paragraph);
+}
+
+function capitalize(string) {
+    let array = string.split('');
+    array[0] = array[0].toUpperCase();
+    let result = "";
+    for(let i = 0; i < array.length; i++) {
+        result += array[i];
+    }
+    return result;
 }
 
 function checkForWinner(npc, player) {
+    const logs = document.querySelector(".logs");
+
     if(npc === player) {
-        return "It's a draw!"
+        createParagraph("It's a draw!", logs);
     } else if(player === "rock" && npc === "scissors" ||
             player === "scissors" && npc === "paper" ||
             player === "paper" && npc === "rock") {
-        return "You win! Rock beats scissors";
+        createParagraph(`You win! ${capitalize(player)} beats ${npc}!`, logs);
+        playerScore += 1;
     } else {
-        return `You lose! ${npc} beats ${player}!`;
+        createParagraph(`You lose! ${capitalize(npc)} beats ${player}!`, logs);
+        npcScore += 1;
     }
 }
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener('click', playRound);
+});
