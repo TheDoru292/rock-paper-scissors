30b8ef8a7d48c4098379d7bc89ffd6db69f90577
TheDoru <thedoruv2@gmail.com>
2022-06-19 14:58:06 +0200

    Finished RPS in Console

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
-# rock-paper-scissors
-A very basic Rock Paper Scissors game made in Javascript and can be played using the Browser's console.
+# Rock Paper Scissors
+This is the new rewrote version of my Rock Paper Scissors project.
 
-# Todo
-- Make it so you can play it in the browser. (COMPLETED)
-- Give it a decent design using CSS.
-- Rewrite the whole JS file. (COMPLETED)
-- Create a seperate file for the Javascript code. (COMPLETED)
-
-These will be made at a later date.
---
+++ script.js
@@ -0,0 +1,52 @@
+function computerPlay() {
+    let number = Math.floor(Math.random() * 3);
+    let choice = determinePlay(number);
+    return choice;
+}
+
+function determinePlay(number) {
+    switch (number) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "scissors";
+            break;
+        case 2:
+            return "paper";
+            break;
+        default:
+            return "Something went wrong.";
+    }
+}
+
+function playerChoice() {
+    let choice = prompt("What would you like to choose? Rock, paper or scissors").toLowerCase();
+    return choice;
+}
+
+function playRound() {
+    let npcChoice = computerPlay();
+    let userChoice = playerChoice();
+    alert(checkForWinner(npcChoice, userChoice));
+}
+
+function checkForWinner(npc, player) {
+    if(npc === player) {
+        return "It's a draw!"
+    } else if(player === "rock" && npc === "scissors" ||
+            player === "scissors" && npc === "paper" ||
+            player === "paper" && npc === "rock") {
+        return "You win! Rock beats scissors";
+    } else {
+        return `You lose! ${npc} beats ${player}!`;
+    }
+}
+
+function game() {
+    for(let i = 0; 5 > i; i++) {
+        playRound();
+    }
+}
+
+game();
